8a11b887ed7f942a551632e26bd7d974c1d56241
Lully <lully1804@yahoo.fr>
2018-01-22 22:11:37 +0100

    Mise à jour de l'exécutable

--- setup_transbiblio.py
+++ setup_transbiblio.py
@@ -5,7 +5,7 @@ from cx_Freeze import setup, Executable
 import os
 
 
-path_anaconda = r"D:\BNF0017855\Programmes\Anaconda2"
+path_anaconda = r"C:\ProgramData\Anaconda3"
 if (path_anaconda[-1] != "\\"):
     path_anaconda = path_anaconda + "\\"
 
