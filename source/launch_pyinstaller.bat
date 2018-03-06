a827a292de78ebf7968e71abef1d7ebe2e5f2f36
Lully <lully1804@yahoo.fr>
2018-03-06 17:58:08 +0100

    Amélioration automatisation compilation

--- source/launch_pyinstaller.bat
+++ source/launch_pyinstaller.bat
@@ -2,9 +2,11 @@
 :: 2. Nettoie le répertoire build, renomme dist en "alignement-donnees-bnf" et y place le raccourci pour le lancement du fichier
 :: 3. Compresse le répertoire obtenu
 :: 4. Supprime le répertoire initial "alignement-donnees-bnf" 
+@echo off
+set /p version="version: "
 pyinstaller main.py
 rd /s /q build
 copy alignement-donnees-bnf.bat dist
 rename dist alignement-donnees-bnf
-"C:\Program Files\7-Zip\7z" a -tzip ..\bin\alignement-donnees-bnf_X.XX_win64_py3.6.zip alignement-donnees-bnf/
+"C:\Program Files\7-Zip\7z" a -tzip ..\bin\alignement-donnees-bnf_%version%_win64_py3.6.zip alignement-donnees-bnf/
 rd /s /q alignement-donnees-bnf
