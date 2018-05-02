8838b0e4e7f5b2dc66ae72dff69c5fb00e78d46f
Lully <lully1804@yahoo.fr>
2018-05-02 14:03:06 +0200

    Compilation 1.16

---
+++ source/launch_pyinstaller_win64.bat
@@ -0,0 +1,12 @@
+:: 1.Lance la compilation du code source
+:: 2. Nettoie le répertoire build, renomme dist en "bibliostratus" et y place le raccourci pour le lancement du fichier
+:: 3. Compresse le répertoire obtenu
+:: 4. Supprime le répertoire initial "bibliostratus" 
+@echo off
+set /p version="version: "
+pyinstaller main.py
+rd /s /q build
+copy bibliostratus.bat dist
+rename dist bibliostratus
+"C:\Program Files\7-Zip\7z" a -tzip ..\bin\bibliostratus_%version%_win64_py3.6.zip bibliostratus/
+rd /s /q bibliostratus
