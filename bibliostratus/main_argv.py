3abe89964130cbcf6222e494b018a843e6e33196
Lully <lully1804@yahoo.fr>
2019-06-19 21:03:51 +0200

    Lancement de BBS en ligne de commande
    Documentation dans le code des erreurs et des paramètres possibles (à continuer)

--- bibliostratus/main_argv.py
+++ bibliostratus/main_argv.py
@@ -6,7 +6,6 @@ Lancer les modules de Bibliostratus en ligne de commande
 On lance chaque module par son nom de fichier (bib2id, aut2id, etc.) avec les paramètres de chaque fonction
 dans leur ordre d'apparition sur le formulaire
 On peut rajouter le repertoire de destination comme dernier argument. Il faut alors le faire preceder de "--"
-Les valeurs "None" viennent remplacer les objets "Formulaires" utilisés par les fonctions launch de chaque module
 
 python main_argv.py aut2id "main/examples/aut_align_aut.tsv" 1 1 1 0 1 argv_test_aut2id 1
 python main_argv.py bib2id "main/examples/mon_impr.tsv" 1 1 0 1 0 argv_test_bib2id
@@ -23,16 +22,68 @@ import bib2id
 import aut2id
 import ark2records
 
-
 dic_functions = {
-    "bib2id": bib2id,
-    "aut2id": aut2id,
-    "marc2tables": marc2tables,
-    "ark2records": ark2records
-}
+    "bib2id": {"action": bib2id,
+               "description": "Alignement de notices bibliographiques",
+               "parametres":
+                [["Nom du fichier", "Chemin (relatif ou absolu) du fichier en entrée à utiliser"],
+                 ["Type de documents", "Valeurs autorisées :\n\
+     1 (TEX)\n\
+     2 (VID)\n\
+     3 (AUD)\n\
+     4 (PER)\n\
+     5 (CAR)\n\
+     6 (PAR)"]
+                ]
+              },
+    "aut2id": {"action": aut2id,
+               "description": "Alignement de notices d'autorité",
+               "parametres": 
+                [["Nom du fichier", "Chemin (relatif ou absolu) du fichier en entrée à utiliser"],
+                 ["Type de notices", "Valeurs autorisées :\n\
+     1 (PERS),\n\
+     2 (ORG),\n\
+     3 (notices BIB pour alignement d'autorités),\n\
+     4 (Rameau)"]
+                ]
+              },
+    "marc2tables": {"action": marc2tables,
+                    "description": "Conversion d'un fichier MARC en plusieurs fichiers tabulés",
+                    "parametres":
+                        [["Nom du fichier", "Chemin (relatif ou absolu) du fichier en entrée à utiliser"],
+                         ["Format du fichier", "Valeurs autorisées :\n\
+     1 (iso2709 en UTF-8)\n\
+     2 (iso2709 en 8859-1)\n\
+     3 (XML en UTF-8)"],
+                         ["Type de notices dans le fichier", "Valeurs autorisées:\n\
+     1 (BIB)\n\
+     2 (AUT)\n\
+     3 (BIB pour alignement AUT)"]
+                        ]
+                    },                     
+    "ark2records": {"action": ark2records,
+                    "description": "Conversion d'une liste d'identifiants BnF (ARK) ou Abes (PPN) en fichier de notices MARC",
+                    "parametres":
+                        [["Nom du fichier", "Chemin (relatif ou absolu) du fichier en entrée à utiliser"],
+                         ["Format du fichier", "Valeurs autorisées :\n     1 (iso2709 en UTF-8)\n\
+     2 (iso2709 en 8859-1)\n     3 (XML en UTF-8)"],
+                         ["Type de notices dans le fichier", "Valeurs autorisées:\n     1 (BIB)\n     2 (AUT)"]
+                        ]
+                    }
+    }
+
+functions_description = [" : ".join([key,  dic_functions[key]['description']]) 
+                         for key in dic_functions]
+error_function_name = "Les fonctions prévues par le programme sont : \n  " + '\n  '.join(functions_description)
 
 if __name__ == "__main__":
-    function = dic_functions[sys.argv[1]].launch
+    function_name = sys.argv[1]
+    try:
+        function = dic_functions[function_name]["action"].launch
+    except KeyError:
+        print("\nErreur : nom de fonction appelée = ", function_name, "\n")
+        print(error_function_name)
+        sys.exit()
     parametres = sys.argv[2:]
     # Si le dernier argument commence par un chevron > : c'est le répertoire de destination
     # pour les rapports
@@ -45,5 +96,13 @@ if __name__ == "__main__":
         i += 1
     try:
         function(*parametres)
-    except ValueError as err:
-        print(err)
+    except TypeError as err:
+        # print(err)
+        print("Fonction appelée : ", function_name)
+        print(dic_functions[function_name]["description"])
+        print("Liste des paramètres : ")
+        i = 1
+        for el in dic_functions[function_name]["parametres"]:
+            print("  ", i, ".", el[0], ":", el[1])
+            i += 1
+        sys.exit()
