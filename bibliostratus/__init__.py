8c21b54f44067a7a5d3829ebc17ed2239ff2cc4a
Lully <lully1804@yahoo.fr>
2020-01-16 13:46:52 +0100

    Fichier __init__

--- bibliostratus/__init__.py
+++ bibliostratus/__init__.py
@@ -1,9 +1,27 @@
 # coding: utf-8
 
+import codecs
+import os
+import json
+import re
+import tkinter as tk
+import webbrowser
+from tkinter import filedialog
+from urllib import error, request
+
+from unidecode import unidecode
+
+import main
 import marc2tables
 import bib2id
 import aut2id
 import ark2records
+import funcs
+import forms
+
+import mapping_number_letters
+import sru
+import udecode
 
 version = 1.28
 lastupdate = "16/01/2019"
