d28d663d1383edfd713a6ff2f88092fe304aae5c
Lully <lully1804@yahoo.fr>
2021-05-28 12:24:29 +0200

    Non-import de pkg_resources
    Mise à jour Python qui faisait planter cet import

--- bibliostratus/__init__.py
+++ bibliostratus/__init__.py
@@ -10,7 +10,7 @@ import os
 import json
 import re
 import smc.bibencodings
-from pkg_resources import py2_warn
+#from pkg_resources import py2_warn
 import tkinter as tk
 import webbrowser
 from tkinter import filedialog
