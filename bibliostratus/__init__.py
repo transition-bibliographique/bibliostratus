84ac212f2a9fcc3c1be0c93171d5b9a3a2bf74b9
Lully <lully1804@yahoo.fr>
2021-11-25 14:56:51 +0100

    Correction n° de version 1.33

--- bibliostratus/__init__.py
+++ bibliostratus/__init__.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 
 version = 1.33
-version_suffix = "3"   # contenu : "RC1", "RC2", "RC3", etc.
-lastupdate = "05/11/2021"
+version_suffix = ""   # contenu : "RC1", "RC2", "RC3", etc.
+lastupdate = "25/11/2021"
 programID = "bibliostratus"
 
 
