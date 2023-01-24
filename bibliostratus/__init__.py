ad2305b098604d17eb9e33b7d26482f81ee443a6
Lully <lully1804@yahoo.fr>
2023-01-24 10:43:15 +0100

    Compilation de la version 1.35 pour publication
    Mise en production de la 1.35

--- bibliostratus/__init__.py
+++ bibliostratus/__init__.py
@@ -2,8 +2,8 @@
 # coding: utf-8
 
 version = 1.35
-version_suffix = "RC1"   # contenu : "RC1", "RC2", "RC3", etc.
-lastupdate = "30/08/2022"
+version_suffix = ""   # contenu : "RC1", "RC2", "RC3", etc.
+lastupdate = "24/01/2023"
 programID = "bibliostratus"
 
 
