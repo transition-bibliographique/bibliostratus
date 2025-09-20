79a0d5f1bbf4ec939fb6ac561dc51b633fb04d04
Etienne CAVALIE <BNF0017855@pfo.nt.bnf.fr>
2025-09-20 11:04:43 +0200

    Compilation de la 1.37RC3

--- bibliostratus/launch_pyinstaller_win64.bat
+++ bibliostratus/launch_pyinstaller_win64.bat
@@ -1,4 +1,4 @@
-:: 1.Lance la compilation du code source
+:: Préalable : lancer la compilation depuis l'environnement virtuel
 :: 2. Nettoie le répertoire build, renomme dist en "bibliostratus" et y place le raccourci pour le lancement du fichier
 :: 3. Compresse le répertoire obtenu
 :: 4. Supprime le répertoire initial "bibliostratus" 
@@ -6,7 +6,7 @@
 set /p version_bbs="version: "
 ::pyinstaller main.py --exclude-module numpy --exclude-module pandas --exclude-module scipy  --exclude-module notebook  --exclude-module matplotlib
 ::pyinstaller main.py
-pyinstaller main.spec
+::pyinstaller main.spec
 rd /s /q build
 copy bibliostratus.bat dist
 xcopy /S main\files dist\main\files\
@@ -22,7 +22,7 @@ set RC=RC
 call set Replaced=%%version:%RC%=%%
 If NOT "%version%"=="%Replaced%" (
     echo version RC
-	copy ..\bin\bibliostratus_%version%_win64_py3.10.zip ..\bin\RC\bibliostratus_%version%_win64_py3.10.zip
+	copy ..\bin\bibliostratus_%version%_win64_py3.10.zip ..\bin\RC\bibliostratus_%version_bbs%_win64_py3.10.zip
 	del ..\bin\bibliostratus_%version%_win64_py3.10.zip
 ) else (
      ::Version en prod
